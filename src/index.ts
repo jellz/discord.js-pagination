245c02ba546bffec4f58e2f6cc57469f44cafe45
Daniel Gulic <danielgulic@gmail.com>
2020-07-14 14:56:44 +1000

    Don't add reactions if there is only 1 page

--- src/index.ts
+++ src/index.ts
@@ -29,34 +29,36 @@ export async function editMessageWithPaginatedEmbeds(
 		pages[page].setFooter(formatFooter(options.footer, page + 1, pages.length))
 	);
 
-	for (const emoji of options.emojiList) await currentPage.react(emoji);
-	const reactionCollector = currentPage.createReactionCollector(
-		(reaction, user) =>
-			options.emojiList.includes(reaction.emoji.name) &&
-			!user.bot &&
-			(options.owner ? options.owner.id === user.id : true),
-		{ time: options.timeout }
-	);
-	reactionCollector.on('collect', (reaction, user) => {
-		reaction.users.remove(user);
-		switch (reaction.emoji.name) {
-			case options.emojiList[0]:
-				page = page > 0 ? --page : pages.length - 1;
-				break;
-			case options.emojiList[1]:
-				page = page + 1 < pages.length ? ++page : 0;
-				break;
-			default:
-				break;
-		}
-		currentPage.edit(
-			pages[page].setFooter(
-				formatFooter(options.footer, page + 1, pages.length)
-			)
+	if (pages.length > 1) {
+		for (const emoji of options.emojiList) await currentPage.react(emoji);
+		const reactionCollector = currentPage.createReactionCollector(
+			(reaction, user) =>
+				options.emojiList.includes(reaction.emoji.name) &&
+				!user.bot &&
+				(options.owner ? options.owner.id === user.id : true),
+			{ time: options.timeout }
 		);
-	});
+		reactionCollector.on('collect', (reaction, user) => {
+			reaction.users.remove(user);
+			switch (reaction.emoji.name) {
+				case options.emojiList[0]:
+					page = page > 0 ? --page : pages.length - 1;
+					break;
+				case options.emojiList[1]:
+					page = page + 1 < pages.length ? ++page : 0;
+					break;
+				default:
+					break;
+			}
+			currentPage.edit(
+				pages[page].setFooter(
+					formatFooter(options.footer, page + 1, pages.length)
+				)
+			);
+		});
 
-	reactionCollector.on('end', () => currentPage.reactions.removeAll());
+		reactionCollector.on('end', () => currentPage.reactions.removeAll());
+	}
 
 	return currentPage;
 }
